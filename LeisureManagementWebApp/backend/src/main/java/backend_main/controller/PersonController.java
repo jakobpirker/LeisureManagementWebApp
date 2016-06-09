6c7000ae6a1cd75c7dcfa8cb898a7ec097575dad
Jakob P <fake.mail@gmail.com>
2016-06-09 10:58:48 +0200

    Backend: - used abstract classes for Person too

--- LeisureManagementWebApp/backend/src/main/java/backend_main/controller/PersonController.java
+++ LeisureManagementWebApp/backend/src/main/java/backend_main/controller/PersonController.java
@@ -10,24 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/person")
-public class PersonController{
-
-    @Autowired
-    protected PersonService service_;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String getRequest() {
-        return service_.addAvailableForeignIdsToPerson();
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public Person postRequest(@RequestBody Person new_person) {
-        new_person = this.service_.save(new_person);
-        return new_person;
-    }
-
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public Iterable<Person> getList() {
-        return service_.getList();
-    }
+public class PersonController extends AbstractController<Person, PersonService>{
 }
--- LeisureManagementWebApp/backend/src/main/java/backend_main/services/PersonService.java
+++ LeisureManagementWebApp/backend/src/main/java/backend_main/services/PersonService.java
@@ -2,6 +2,7 @@ package backend_main.services;
 
 import backend_main.entities.Address;
 import backend_main.entities.Person;
+import backend_main.entities.embedded_ids.PersonId;
 import backend_main.repositories.AddressRepository;
 import backend_main.repositories.PersonRepository;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -14,14 +15,12 @@ import org.springframework.stereotype.Service;
 import java.util.Iterator;
 
 @Service
-public class PersonService {
-
-    @Autowired
-    private PersonRepository person_repository_;
+public class PersonService extends AbstractService<Person, PersonId, PersonRepository>{
 
     @Autowired
     private AddressRepository address_repository_;
 
+    @Override
     public Person save(Person save_person){
 
         // try to insert the valid address-object from the DB by it's id (from JSON)
@@ -29,24 +28,18 @@ public class PersonService {
         {
             save_person.setAddress(address_repository_.findOne(save_person.getAddressId()));
         }
-        return this.person_repository_.save(save_person);
+        return this.base_repository_.save(save_person);
     }
 
-    public Iterable<Person> getList(){
-        return person_repository_.findAll();
-    }
+    public String getJsonStringObjectWithForeignIds() {
 
-    public String addAvailableForeignIdsToPerson() {
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        JsonNode person = mapper.valueToTree(new Person());
-        JsonNode address_ids = mapper.createArrayNode();
+        JsonNode person = object_mapper_.valueToTree(new Person());
+        JsonNode address_ids = object_mapper_.createArrayNode();
 
         Iterable<Address> addresses = address_repository_.findAll();
 
         for(Iterator<Address> i = addresses.iterator(); i.hasNext(); ) {
-            ((ArrayNode) address_ids).add(mapper.valueToTree(i.next().getId()));
+            ((ArrayNode) address_ids).add(object_mapper_.valueToTree(i.next().getId()));
         }
 
         ((ObjectNode) person).put("Adresse", address_ids);
